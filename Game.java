aa50e17893b8ac923db92f95b694cfe512f890fe
sweinstein <sweinstein@student-bmhs-la.org>
2023-01-06 12:44:40 -0800

    second push and commit

--- Game.java
+++ Game.java
@@ -1,13 +1,26 @@
+import java.util.Scanner;
 public class Game {
     private String hidden;
     private String guess;
     private String hint;
+    private Scanner kb;
     public static void main (String[] args) {
         Game game = new Game();
         game.run();
     }
     public void run(){
-       System.out.println("sam");
-    }
+        System.out.println("Choose a word.") ;
+        String hidden = kb.nextLine();
+        while(hidden!= guess) {
+            System.out.println("Guess a" + hidden.length() + " letter word.");
+            String guess = kb.nextLine();
 
+     if (substring.equals -1)
+        System.out.println("*");
+     if(substring > -1) {
+       System.out.println("+");
+     } else
+         System.out.println(substring);
+        }
+    }
 }
