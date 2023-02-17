52d9fbb2da4d54d52eb46ad3cfd3704ba5e3d4c3
sweinstein <sweinstein@student-bmhs-la.org>
2023-02-17 09:47:22 -0800

    finished

--- Game.java
+++ Game.java
@@ -3,24 +3,41 @@ public class Game {
     private String hidden;
     private String guess;
     private String hint;
-    private Scanner kb;
-    public static void main (String[] args) {
+
+    public static void main(String[] args) {
         Game game = new Game();
         game.run();
-    }
-    public void run(){
-        System.out.println("Choose a word.") ;
-        String hidden = kb.nextLine();
-        while(hidden!= guess) {
-            System.out.println("Guess a" + hidden.length() + " letter word.");
-            String guess = kb.nextLine();
 
-     if (substring.equals -1)
-        System.out.println("*");
-     if(substring > -1) {
-       System.out.println("+");
-     } else
-         System.out.println(substring);
+    }
+
+    public void run() {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Enter a word my boy");
+        hidden = kb.nextLine();
+
+        int hiddenLength = hidden.length();
+
+        System.out.println(hidden);System.out.println(guess);
+
+
+        while (!hidden.equals(guess))
+        {
+            hint = "";
+            System.out.println("Enter a " + hiddenLength + " letter word");
+            guess = kb.nextLine();
+
+            for (int i = 0; i < hidden.length(); i++) {
+                if (hidden.substring(i, i + 1).equals(guess.substring(i, i + 1))) {
+                    hint += guess.substring(i, i + 1);
+                } else if (hidden.indexOf(guess.substring(i, i + 1)) != -1) {
+                    hint += "*";
+                } else {
+                    hint += "*";
+                }
+
+
+            }
+            System.out.println(hint);
         }
     }
 }
